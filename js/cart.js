068d195a70f0d82f59ee72839cc0ec388fca41dd
Kris Sakarias <ksakarias@gmail.com>
2018-01-25 17:15:48 -0800

    rendered items in cart

--- js/cart.js
+++ js/cart.js
@@ -1,8 +1,30 @@
 'use strict';
 var cartProducts = [];
+var ulEl = document.getElementById('cart-list');
 
 
 function displayProduct() {
   cartProducts = JSON.parse(localStorage.getItem( 'chosenProducts' ));
+  for (var i in cartProducts) {
+    var liEl = document.createElement('li');
+    var imgEl = document.createElement('img');
+    var liButton = document.createElement('button');
+    var spanEl = document.createElement('span');
+    var pName = document.createElement('p');
+    
+    
+    pName.textContent = cartProducts[i].name;
+    spanEl.textContent = ' Quantity: ' + cartProducts[i].quantitySelected;
+    imgEl.src = cartProducts[i].filepath;
+    liButton.textContent = 'Delete Item';
+    // liEl.textContent = cartProducts[i].name;
+
+    liEl.appendChild(imgEl);
+    liEl.appendChild(pName);
+    liEl.appendChild(spanEl);
+    liEl.appendChild(liButton);
+    ulEl.appendChild(liEl);
+
+  }
 }
 displayProduct();
